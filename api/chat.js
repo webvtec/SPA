786a32d4785c3977febddf3eacfe739c8c4695b9
webvtec <webvtec8@gmail.com>
2025-09-11 15:09:43 -0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // preflight
+  }
+
   try {
     const { inputs } = req.body;
 
