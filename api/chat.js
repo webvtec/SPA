1004413fcd6ee98dfceb8237124babe6ac7a3f13
webvtec <webvtec8@gmail.com>
2025-09-11 15:50:10 -0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,3 +1,4 @@
+// api/chat.js
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
